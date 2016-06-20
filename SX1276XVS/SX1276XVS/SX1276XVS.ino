fdbcdbf90e8ae21bbd79edfd96a971b801ec90f9
David-CK <zhiweid@126.com>
2016-06-20 18:03:54 +0800

    class radio

--- SX1276XVS/SX1276XVS/SX1276XVS.ino
+++ SX1276XVS/SX1276XVS/SX1276XVS.ino
@@ -1,15 +1,18 @@
-/*
- Name:		SX1276XVS.ino
- Created:	6/20/2016 5:15:33 PM
- Author:	dongz1x
-*/
+#include "./radio/radio.h"
+
+Radio Radio(1);
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-
+void setup()
+{
+    // verify the connection with the board
+   // while( Radio.Read( REG_VERSION ) == 0x00  )
+    {
+    }
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
+void loop()
+{
   
 }
